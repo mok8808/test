d8084ca545b60ccfd12af36522c0eec5c2d2defd
mok8808 <mok8808@sina.com>
2020-12-15 21:26:55 +0800

    first commit

--- pandas_笔记/some_module.py
+++
@@ -1,6 +0,0 @@
-# some_module.py（系统内部模块）
-PI = 3.14159
-def f(x):
-    return x + 5
-def g(a, b):
-    return a + b
--- pandas_笔记/some_mok.py
+++
@@ -1,5 +0,0 @@
-# some_mok.py (这些代码要做python下专门新建一个some_mok.py文件并放在当前文件夹下才能调用)
-def f(x):
-    return x + 10
-def g(a, b):
-    return a + b
